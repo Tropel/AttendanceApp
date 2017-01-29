adf799e1b5aec5cecfbeabb6840d0e0df6decb3f
Marcel Toeltl <marcel.toeltl@gmail.com>
2017-01-29 16:24:55 +0100

    Hinzugefügt

--- app/src/main/java/ch/chost/attendanceapp/activities/MainActivity.java
+++ app/src/main/java/ch/chost/attendanceapp/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package ch.chost.attendanceapp;
+package ch.chost.attendanceapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ch.chost.attendanceapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
