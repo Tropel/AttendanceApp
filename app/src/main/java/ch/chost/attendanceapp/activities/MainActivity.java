2e65be3cb69c4524bb6fe2b4b4854722c2756309
Marcel Toeltl <marcel.toeltl@gmail.com>
2017-02-02 21:21:51 +0100

    Implemented Toolbar, and we added RecyclerView and CircleImageView dependencies

--- app/src/main/java/ch/chost/attendanceapp/activities/MainActivity.java
+++ app/src/main/java/ch/chost/attendanceapp/activities/MainActivity.java
@@ -2,14 +2,20 @@ package ch.chost.attendanceapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import ch.chost.attendanceapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
 }
